ba48ebf3823f803485dacdbc3fb6d832fb2f77b3
Alabi A <alabi10@yahoo.com>
2019-01-12 15:25:54 +0100

    changed to using template file index.html

--- web-app/application.py
+++ web-app/application.py
@@ -1,13 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello World"
-
-@app.route("/<string:name>")
-def hello(name):
-    name = name.capitalize()
-    return f"Hello, {name}!"
-        
+    headline = "Hello World"
+    return render_template("index.html", headline=headline)     
